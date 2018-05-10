d3d044b844a57347d56123d117399d52716b19b6
Leonardo Mariano Castro González <lmcg1191@gmail.com>
2018-05-09 21:33:33 -0500

    change senses

--- src/Highway.jl
+++ src/Highway.jl
@@ -11,11 +11,9 @@ export
         Pos_next, Pos_right, Pos_left, Pos_left9,
         Merge!, Merge_Left_Right!, Merge_Right_Left!, safe_distance, Ramp!,
         AccelerationNoise, DecelerationMove, AccelerationNoiseS1, AccelerationNoiseS2,
-        Sense1, Sense2, S1_in_ramp, S1_out_ramp, S2_out_ramp, Sections1, Sections2, S1_P_camion_s1, S1_P_camion_s2,
-        S1_in_ramp_ampli, Sections1_CR, Sections2_CR,
-        P_ramp_S1, P_ampli_S1,
         Measure_Fluxes!, Measure_Speeds!, add_Fluxes!, add_Speeds!, Measure_Densities!, Measure!, Measure_s!,
-        S1_ramp, S1_ramp_ampli, S1_ramp_local, P_local_S1,
+        Sense1, Sense2, Sections1_CR, Sections2_CR,
+        S1_ramp, S1_ramp_ampli, S1_ramp_local, P_ramp_S1, P_ampli_S1, P_local_S1,
         S2_ramp, S2_ramp_ampli, S2_ramp_local, P_ramp_S2, P_ampli_S2, P_local_S2
 
 
